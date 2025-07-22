e50c9493560e3ef3ffb1e33dde5c80598b39af34
nageshwarp1997 <nageshwar@ctruh.com>
2025-07-22 06:15:15 +0530

    Bar Graph Updated

--- src/components/dashboard/BarGraph.jsx
+++ src/components/dashboard/BarGraph.jsx
@@ -8,21 +8,47 @@ import {
   Tooltip,
   Legend,
 } from "chart.js";
-import { formatSalaryRange } from "../../utils";
+// import { formatSalaryRange } from "../../utils";
+function formatSalaryRange(salary) {
+  const salaryInK = Math.floor(salary / 100) * 10;
+  if (salaryInK < 10000) return "0-10k";
+  if (salaryInK < 20000) return "10k-20k";
+  if (salaryInK < 30000) return "20k-30k";
+  if (salaryInK < 40000) return "30k-40k";
+  if (salaryInK < 50000) return "40k-50k";
+  if (salaryInK < 60000) return "50k-60k";
+  if (salaryInK < 70000) return "60k-70k";
+  if (salaryInK < 80000) return "70k-80k";
+  if (salaryInK < 90000) return "80k-90k";
+  return "90k-99k+";
+}
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
 const BarGraph = ({ candidates }) => {
   console.log("candidates", candidates[0]);
 
-  const salariesByRange = {};
+  const salariesByRange = {
+    "0-10k": 0,
+    "10k-20k": 0,
+    "20k-30k": 0,
+    "30k-40k": 0,
+    "40k-50k": 0,
+    "50k-60k": 0,
+    "60k-70k": 0,
+    "70k-80k": 0,
+    "80k-90k": 0,
+    "90k-99k+": 0,
+  };
 
   candidates.forEach((candidate) => {
     const salaryRange = formatSalaryRange(candidate.estimated_salary);
-    if (salariesByRange[salaryRange]) {
+    console.log("candidate.estimated_salary", candidate.estimated_salary);
+    if (Object.prototype.hasOwnProperty.call(salariesByRange, salaryRange)) {
       salariesByRange[salaryRange]++;
     } else {
-      salariesByRange[salaryRange] = 1;
+      // Just in case formatSalaryRange returns a value not defined in the map
+      salariesByRange["90k-99k+"]++;
     }
   });
 
